886ba24b3e132230676b9fd4702bcb4fe5dacea6
Anti-Gen <putrategu@gmail.com>
2023-02-06 20:53:00 +0700

    bljr

---
+++ VAlidparenthese.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	if len(s) <= 1 {
+		return false
+	}
+	m := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+	var stack []rune
+
+	for _, v := range s {
+		if v == '(' || v == '[' || v == '{' {
+
+			stack = append(stack, v)
+		} else {
+			lastIndex := len(stack) - 1
+			if lastIndex == -1 {
+				return false
+			}
+			lastOpenParenthese := stack[lastIndex]
+			if v == m[lastOpenParenthese] {
+				stack = stack[:lastIndex]
+			} else {
+				return false
+			}
+		}
+
+	}
+
+	return len(stack) == 0
+}
+
+func main() {
+
+	fmt.Println(isValid("{(})"))
+
+}
---
+++ cob.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "teguh"
+
+	fmt.Println(str[:0])
+
+}
